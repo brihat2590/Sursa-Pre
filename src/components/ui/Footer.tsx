86b429b3905814d571d0c964b7f7bc4d571c01dd
brihat2590 <thapabrihatbir321@gmail.com>
2025-07-25 17:42:12 +0530

    update phone

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -74,7 +74,7 @@ export default function Footer() {
                       d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                     />
                   </svg>
-                  <a href="tel:+977-1-1234567">+977-1-1234567</a>
+                  <a href="tel:+977-(01)-4543720">+977-(01)-4543720</a>
                 </p>
               </address>
             </div>
@@ -104,7 +104,7 @@ export default function Footer() {
               </form>
               <div className="mt-6">
                 <a
-                  href="mailto:info@sursa.tech"
+                  href="mailto:info@sursatech.com"
                   className="text-sm flex items-center hover:text-opacity-80"
                   style={{ color: "#ad9665" }}
                 >
@@ -123,7 +123,7 @@ export default function Footer() {
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  info@sursa.tech
+                  info@sursatech.com
                 </a>
               </div>
             </div>
