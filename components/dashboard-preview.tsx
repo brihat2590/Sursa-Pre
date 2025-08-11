bf7aa19ff5b44551b90ea77f89e8224e7d902c9e
brihat2590 <thapabrihatbir321@gmail.com>
2025-08-11 11:14:22 +0530

    changed img

--- components/dashboard-preview.tsx
+++ components/dashboard-preview.tsx
@@ -4,7 +4,7 @@ export function DashboardPreview() {
   return (
     <div className="w-[calc(100vw-32px)] md:w-[1160px] mt-10">
       <div className="bg-primary-light/50 rounded-2xl p-2 shadow-2xl sm:mb-15 ">
-        <Image
+        <img
           src="https://s3.sursatech.com/api/v1/buckets/sursakit/objects/download?preview=true&prefix=website%2F476345147-3448b77b-8c4f-48c9-90e5-2dd0e2477f5d.png"
           alt="Dashboard preview"
           width={1200}
