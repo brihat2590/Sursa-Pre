85c0e61310b08ae293c2171f1cfbfacc68bd8487
brihat2590 <thapabrihatbir321@gmail.com>
2025-08-15 10:12:31 +0530

    update: footer

--- components/footer-section.tsx
+++ components/footer-section.tsx
@@ -3,9 +3,9 @@ import { Phone } from "lucide-react";
 export default function FooterSection() {
   return (
     <footer className=" text-gray-300 py-12 px-6 cursor-pointer">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
+      <div className="max-w-7xl mx-auto flex flex-col gap-8 lg:flex-row lg:items-center justify-evenly ">
         {/* SursaKit info */}
-        <div>
+         <div>
           <div className="flex items-center mb-3">
             
             <span className="ml-2 font-semibold text-white text-lg">SursaKit</span>
@@ -13,10 +13,10 @@ export default function FooterSection() {
           <p className="text-sm leading-relaxed max-w-xs">
             AI-powered cloud editor for the modern developer. Transform your coding experience with intelligent assistance.
           </p>
-        </div>
+        </div> 
 
-        {/* United States */}
-        <div>
+       
+        {/* <div>
           <h4 className="text-white font-semibold mb-3">UNITED STATES</h4>
           <address className="not-italic text-sm leading-relaxed space-y-1">
             <p>Sursa LLC</p>
@@ -27,10 +27,10 @@ export default function FooterSection() {
               +1 (302) 464-6096
             </a>
           </address>
-        </div>
+        </div> */}
 
         {/* Nepal */}
-        <div>
+        {/* <div>
           <h4 className="text-white font-semibold mb-3">NEPAL</h4>
           <address className="not-italic text-sm leading-relaxed space-y-1">
             <p>Sursa Tech Pvt. Ltd.</p>
@@ -41,7 +41,7 @@ export default function FooterSection() {
               +977-(01)-4543720
             </a>
           </address>
-        </div>
+        </div>  */}
 
         {/* Stay Updated */}
         <div>
@@ -50,13 +50,13 @@ export default function FooterSection() {
             If you have any queries, you can mail us at the following mail id.
           </p>
           <a
-            href="mailto:info@sursatech.com"
+            href="mailto:hello@sursakit.com"
             className="flex items-center gap-2  hover:underline"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12l-4-4-4 4m8 0l-4 4-4-4" />
             </svg>
-            info@sursatech.com
+            hello@sursakit.com
           </a>
         </div>
       </div>
