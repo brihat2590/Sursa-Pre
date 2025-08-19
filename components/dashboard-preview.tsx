4225c8f9b242bea0e0a783b1fd9da636b882d0d8
Saurabh Chaulagai <saurabh@sursatech.com>
2025-08-19 08:16:49 +0545

    Updated image

--- components/dashboard-preview.tsx
+++ components/dashboard-preview.tsx
@@ -5,7 +5,7 @@ export function DashboardPreview() {
     <div className="w-[calc(100vw-32px)] md:w-[1160px] mt-10">
       <div className="bg-primary-light/50 rounded-2xl p-2 shadow-2xl sm:mb-15 ">
         <img
-          src="https://s3.sursatech.com/api/v1/buckets/sursakit/objects/download?preview=true&prefix=website%2F476345147-3448b77b-8c4f-48c9-90e5-2dd0e2477f5d.png"
+          src="https://s3.sursatech.com/api/v1/buckets/sursakit/objects/download?preview=true&prefix=website%2Feditor%201.png"
           alt="Dashboard preview"
           width={1200}
           height={800}
